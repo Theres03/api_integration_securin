2ea215c7b74c3c4c117799f3590f94e6513ad51a
RIELA_THERESA_ <142734814+Theres03@users.noreply.github.com>
2024-12-19 20:47:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import CVEList from './CVEList'; 
+
+const App = () => {
+  return (
+      <div className="App">
+        <CVEList />
+
+      </div>
+  );
+};
+
+export default App;
